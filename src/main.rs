a1b7ebbf993fa228a26897a893900b828ea06a66
Thiago Oliveira <thiago@signaes.com>
2024-01-21 19:33:08 -0300

    Add basic support for formating

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
-use std::time::SystemTime;
+use chrono::{DateTime, Utc};
+use std::{env, time::SystemTime, usize};
 
 fn main() {
-    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => println!("{}", n.as_millis()),
+    let args: Vec<String> = env::args().collect();
+    let now = SystemTime::now();
+    let mut fmt = false;
+
+    if &args.len().wrapping_add_signed(-1) > &usize::MIN {
+        if &args[1] == "--fmt" {
+            fmt = true
+        }
+    }
+
+    match now.duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => {
+            if fmt {
+                let now: DateTime<Utc> = Utc::now();
+
+                println!("{}", now.format("%Y%m%d%H%M%S%3f"))
+            } else {
+                println!("{}", n.as_millis())
+            }
+        }
         Err(err) => panic!("{:?}", err),
     }
 }
